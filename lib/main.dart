06eb34304d0d98ecdd1ffedc4de9701427981bf9
dabuzida <dabuzida@gmail.com>
2022-12-06 16:27:03 +0900

    start

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'test_switch.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Grammar',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Grammar'),
+          elevation: 0,
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.teal,
+          centerTitle: true,
+        ),
+        body: const TestSwitch(),
+      ),
+    );
+  }
+}
---
+++ lib/test_switch.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestSwitch extends StatefulWidget {
+  const TestSwitch({super.key});
+
+  @override
+  State<TestSwitch> createState() => _TestSwitchState();
+}
+
+class _TestSwitchState extends State<TestSwitch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
